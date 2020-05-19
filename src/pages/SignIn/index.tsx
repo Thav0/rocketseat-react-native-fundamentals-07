009f41727a57bbcc989d12819620c9998eb68b20
Gustavo Diniz <gustavodnz035@gmail.com>
2020-05-19 08:13:24 -0300

    Border focus

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,12 +6,14 @@ import {
   KeyboardAvoidingView,
   Platform,
   TextInput,
+  Alert,
 } from 'react-native';
+import * as Yup from 'yup';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
-
+import getValidationErrors from '../../utils/getValidationErrors';
 import {
   Container,
   Title,
@@ -26,13 +28,55 @@ import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
 
-  const handleSignIn = useCallback((data: object) => {
-    console.log(data);
+  const handleSignIn = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Digite um email valido')
+          .required('E-mail obrigatório'),
+        password: Yup.string().required('Password obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      // history.push('/dashboard');
+
+      // await signIn({
+      //   email: data.email,
+      //   password: data.password,
+      // });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const getErrors = getValidationErrors(err);
+
+        formRef.current?.setErrors(getErrors);
+      }
+
+      Alert.alert(
+        'Falha no login',
+        'Ocorreu um erro ao fazer login, verifique seus dados'
+      );
+
+      // disparar um toast
+      // addToast({
+      //   title: 'Falha no login',
+      //   description: 'Email ou senha incorretos',
+      //   type: 'error',
+      // });
+    }
   }, []);
 
   return (
